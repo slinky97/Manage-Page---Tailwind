1041ceb6b377806113c3448f0e23fc22e113b30c
Diego AZ <21963801+slinky97@users.noreply.github.com>
2024-03-07 19:23:37 -0600

    Home Page sin 70%
    Se creo y se finalizaron varios componentes como el header, contenidos del main tanto sus versiones Desktop como Mobile. Falta creacion del footer y slider de What they have to said

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "Vietnam-Pro": ['"Be Vietnam Pro"', "sans-serif"],
+      },
+      colors: {
+        "bright-red": "hsl(12, 88%, 59%)",
+        "dark-blue": "hsl(228, 39%, 23%)",
+        "dark-grayish-blue": "hsl(227, 12%, 61%)",
+        "very-dark-blue": "hsl(233, 12%, 13%)",
+        "very-pale-red": "hsl(13, 100%, 96%)",
+        "vary-light-gray": "hsl(0, 0%, 98%)",
+      },
+      backgroundImage: {
+        "close-menu": "url('../../assets/images/icon-close.svg')",
+        "open-menu": "url('../../assets/images/icon-hamburger.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
